97ec4925cff4127332df8bff62cc1e109bc52a1b
Hitalo <hitalo331@outlook.com>
2020-10-25 02:39:21 +0000

    setup.sh: Added missing packages
    - Added some packages necesseries from Treble-Experience/Firmware_extractor

--- README.md
+++ README.md
@@ -42,5 +42,5 @@ Supported types on Android 10:
 MIUI, OxygenOS, Generic, Pixel, JoyUI, Moto, ZenUI, ZUI, Flyme, ColorOS, VOS, OneUI, RogUI, Nubia
 
 Supported types on Android 11:
-Generic, Pixel, MIUI, OxygenOS
+Generic, Pixel, MIUI, OxygenOS, OneUI
 ```
--- setup.sh
+++ setup.sh
@@ -3,14 +3,21 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
     if [[ "$distro" == "arch" ]]; then
-       echo "Arch Linux Detected"
+       echo "-> Arch Linux Detected"
        sudo pacman -S --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2
        #aur=rar
+       for package in mpack rename; do
+           git clone https://aur.archlinux.org/"${package}"
+           cd "${package}" || continue
+           makepkg -si --skippgpcheck
+           cd - || break
+           rm -rf "${package}"
+       done
     else
        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk aria2 rename -y
     fi
     pip install backports.lzma protobuf pycrypto
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install protobuf xz brotli lz4 aria2
+    brew install protobuf xz brotli lz4 aria2 liblzma-dev
     pip install backports.lzma protobuf pycrypto
 fi
